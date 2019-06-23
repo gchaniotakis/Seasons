54152db847db8a7c2cb10c0cc90b25b267441867
George Chaniotakis <haniotinho@gmail.com>
2019-06-24 00:25:10 +0300

    implementing geolocation api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
-    return <div>Hi there!</div>;
+    window.navigator.geolocation.getCurrentPosition(
+       (position) => console.log(position),
+       (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
 };
 
 
