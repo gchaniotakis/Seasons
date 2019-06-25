3864fd8f04ae37c5e1d8ece5f362f77cfe85d519
George Chaniotakis <haniotinho@gmail.com>
2019-06-26 01:41:07 +0300

    creating icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -15,9 +15,12 @@ const SeasonDisplay = (props) =>{
     const season = getSeason(props.lat, new Date().getMonth());
     console.log(season);
     const text = season === 'winter' ? 'Burr, it is chilly!' : 'Let\'s hit the beach!';
+    const icon = season ==='winter' ? 'snowflake' : 'sun';
     return (
         <div>
+            <i className = {`${icon} icon`}/>
             <h1>{text}</h1>
+            <i className = {`${icon} icon`}/>
         </div>
     );
 };
